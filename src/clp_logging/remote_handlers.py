0ae4a2a446278981568c322843769061b93482ee
IreneLi <irene2002yr@gmail.com>
2025-01-03 14:04:57 -0500

    Create multipart upload initiation

--- src/clp_logging/remote_handlers.py
+++ src/clp_logging/remote_handlers.py
@@ -1,8 +1,9 @@
 import boto3
 from botocore.exceptions import NoCredentialsError
 from clp_logging.handlers import *
+import datetime
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Any, List
 
 class CLPRemoteHandler():
     """
@@ -23,8 +24,31 @@ class CLPRemoteHandler():
         self.remote_folder_path: Optional[str] = None
         self.obj_key: Optional[str] = None
 
+    def _remote_log_naming(self, timestamp: datetime.datetime) -> str:
+        new_filename: str
+        ext: int = self.log_name.find(".")
+        upload_time: str = timestamp.strftime("%Y-%m-%d-%H%M%S")
+
+        if ext != -1:
+            new_filename = f'log_{upload_time}{self.log_name[ext:]}'
+        else:
+            new_filename = f'{upload_time}_{self.log_name}'
+        new_filename = f"{self.remote_folder_path}/{new_filename}"
+        return new_filename
+
     def get_obj_key(self) -> str:
         return self.obj_key
 
     def set_obj_key(self, obj_key) -> None:
         self.obj_key = obj_key
+
+    def initiate_upload(self, log_path: Path) -> None:
+        self.log_path: Path = log_path
+        self.log_name: str = log_path.name
+        timestamp: datetime.datetime = datetime.datetime.now()
+        self.remote_folder_path: str = f'logs/{timestamp.year}/{timestamp.month}/{timestamp.day}'
+
+        self.obj_key: str = self._remote_log_naming(timestamp)
+        create_ret: Dict[str, Any] = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=self.obj_key,
+                                                                            ChecksumAlgorithm='SHA256')
+        self.upload_id = create_ret['UploadId']
